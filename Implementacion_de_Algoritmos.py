40c588463dfef5595cf17254b2b9cc9d755ce811
Hyldia <hyldiahidgson@gmail.com>
2025-08-24 13:40:36 -0600

    Algoritmo de búsqueda
    Se añaden 3 diferentes algoritmos de búsqueda

--- Implementacion_de_Algoritmos.py
+++ Implementacion_de_Algoritmos.py
@@ -107,15 +107,134 @@ def countingSort(lista):
     return lista_ordenada
 
 
+
+
+# Algoritmos de búsqueda
+#1. Búsqueda lineal
+def busqueda_lineal(matriz, numero):
+    """
+    Busca un número dentro de una matriz.
+    
+    paráramteros:
+        matriz (lista): Matriz donde se realizará la búsqueda.
+        numero (int): Número que se desea buscar.
+    
+    Retorna:
+        int: Retorna la posoción de la matriz donde se encuenta el número. Si no lo encuentra retorna -1.
+    """
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            if matriz[i][j] == numero:
+                return (i, j)
+    return -1
+
+
+
+#2. Búsqueda binaria
+def busqueda_binaria(lista, numero):
+    """
+    Busca un número dentro de una lista ordenada.
+    
+    parárametros:
+        lista (lista): Lista donde se realizará la búsqueda.
+        numero (int): Número que se desea buscar.
+        
+    Retorna:
+        int: Retorna la posoción de la lista donde se encuenta el número. Si
+    """
+    izquierda = 0
+    derecha = len(lista) - 1
+    while izquierda <= derecha:
+        medio = (izquierda + derecha) // 2
+        if lista[medio] == numero:
+            return medio
+        elif lista[medio] < numero:
+            izquierda = medio + 1
+        else:
+            derecha = medio - 1
+    return -1
+
+def busqueda_en_matriz(matriz, numero):
+    """
+    Busca un número dentro de una matriz ordenada.
+    
+    parárametros:
+        matriz (lista): Matriz donde se realizará la búsqueda.
+        numero (int): Número que se desea buscar.
+    
+    Retorna:
+        int: Retorna la posoción de la matriz donde se encuenta el número. Si no lo encuentra retorna -1.
+    """
+    for i in range(len(matriz)):
+        indice = busqueda_binaria(matriz[i], numero)
+        if indice != -1:
+            return (i, indice)
+    return -1
+    
+
+#3. Hashing
+def Hashing(matriz, numero):
+    """
+    Busca un número dentro de una matriz.
+    
+    parárametros:
+        matriz (lista): Matriz donde se realizará la búsqueda.
+        numero (int): Número que se desea buscar.
+        
+    Retorna:
+        int: Retorna la posoción de la matriz donde se encuenta el número. Si no lo encuentra retorna -1.
+    """
+    diccionario = {}
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            diccionario[matriz[i][j]] = (i, j)
+    return diccionario.get(numero, -1)
+
+
+
 #Parte de ejecución
-matriz = generateMatrix(500)
-print(f"antes: {matriz}")
+matriz = generateMatrix(50)
+print("\nMatriz Generada:")
+for fila in matriz:
+    print(fila)
+"""
+#Busqueda lineal
+inicio = time.time()
+resultado = busqueda_lineal(matriz, 933)
+fin = time.time()
+print("\nPosición numero (búsqueda lineal):", resultado, "\nTiempo:", fin - inicio)
+"""
+
+"""
+#Busqueda binaria
+inicio = time.time()
+matriz_ordenada = []
+for i in range(len(matriz)):
+    matriz_ordenada.append(countingSort(matriz[i]))
+resultado = busqueda_en_matriz(matriz_ordenada, 30)
+fin = time.time()
+print("\nPosición numero (búsqueda binaria):", resultado, "\nTiempo:", fin - inicio)
+"""
+
+
+#Hashing
+inicio = time.time()
+resultado = Hashing(matriz, 40)
+fin = time.time()
+print("\nPosición numero (Hashing):", resultado, "\nTiempo:", fin - inicio)
+
+
+"""
+#Ordenamiento
 inicio = time.time()
 for i in range(len(matriz)):
-    #matriz[i] = countingSort(matriz[i])
-    matriz[i] = mergeSort(matriz[i])
+    matriz[i] = countingSort(matriz[i])
+    #matriz[i] = mergeSort(matriz[i])
     #quickSort(matriz[i], 0, len(matriz[i]) - 1) 
+#imperimir la matriz ordenada
+print("\nMatriz Ordenada:")
+for fila in matriz:
+    print(fila)
 fin = time.time()
 print("Tiempo:", fin - inicio)
-print("finish")
-#print(f"despues: {matriz}")
+"""
